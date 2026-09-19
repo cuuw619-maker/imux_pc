c85029a059cde1c45b6ecde6bce4daa45cfe8080
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 12:32:40 +0300

    Add core Gradle and domain foundation

---
+++ core/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    testImplementation(kotlin("test"))
+}
+kotlin { jvmToolchain(21) }
