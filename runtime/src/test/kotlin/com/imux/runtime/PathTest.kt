e1f03d08a439384d8b71d67d59f68cc041832cc1
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 18:27:01 +0300

    Remove obsolete project layer

--- runtime/src/test/kotlin/com/imux/runtime/PathTest.kt
+++
@@ -1,4 +0,0 @@
-package com.imux.runtime
-import kotlin.test.Test
-import kotlin.test.assertEquals
-class PathTest { @Test fun rootDirectoryIsImux() { assertEquals("Imux", ImuxPaths.root.fileName.toString()) } }
