d158d3eca0e1133ad26d8b8e1f74fa6030ba8dbc
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 18:07:11 +0300

    Remove obsolete native C++ launcher

--- native/cpp/imux_launcher_functions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <filesystem>
-
-std::filesystem::path imux_launcher_app_data_root();
-std::filesystem::path imux_launcher_find_game_executable();
-int imux_launcher_try_launch_game();
