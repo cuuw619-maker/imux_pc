380db00494e7e9c0d2465f72d81d39243b1e4f2a
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 18:26:37 +0300

    Remove obsolete project layer

--- runtime/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins { kotlin("jvm"); kotlin("plugin.serialization") }
-dependencies {
-    implementation(project(":core"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
-    testImplementation(kotlin("test"))
-}
-kotlin { jvmToolchain(21) }
