0ce469c3eb9304918377a7bd0b9050379fc15f46
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 18:26:32 +0300

    Remove obsolete project layer

--- modding/src/main/kotlin/com/imux/modding/api/ModApi.kt
+++
@@ -1,6 +0,0 @@
-package com.imux.modding.api
-interface ModLoader { fun discover(): List<ModMetadata> }
-data class ModMetadata(val id: String, val name: String, val version: String, val apiVersion: String, val entrypoint: String? = null)
-interface ModContext { val modId: String; fun register(service: Any) }
-interface ModAPI { val version: String }
-interface ModRuntime { fun start(context: ModContext); fun stop() }
