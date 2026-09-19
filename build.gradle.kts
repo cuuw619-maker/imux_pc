3eb0aa80d9c61419919f1bdc71127439a834ad80
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 18:24:08 +0300

    Reset project to minimal Kotlin launcher architecture

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     kotlin("jvm") version "2.1.21" apply false
-    kotlin("plugin.serialization") version "2.1.21" apply false
-    id("org.jetbrains.compose") version "1.8.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
 }
+
 group = "com.imux"
 version = "0.0.1"
+
 subprojects {
     group = rootProject.group
     version = rootProject.version
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
-    tasks.withType<JavaCompile>().configureEach { options.release.set(21) }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.release.set(21)
+    }
 }
