1e9bf9f0cde8eeac49a62d800de638c472f4acc4
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 14:36:21 +0300

    Use Imux icon in launcher window

--- launcher/src/main/kotlin/com/imux/launcher/Main.kt
+++ launcher/src/main/kotlin/com/imux/launcher/Main.kt
@@ -1,10 +1,11 @@
 package com.imux.launcher
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.res.painterResource
 import com.imux.runtime.ImuxPaths
 fun main() {
     ImuxPaths.initialize()
     application {
-        Window(onCloseRequest = ::exitApplication, title = "Imux") { LauncherApp() }
+        Window(onCloseRequest = ::exitApplication, title = "Imux", icon = painterResource("icon.webp")) { LauncherApp() }
     }
 }
