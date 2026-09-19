403ca7796aad6ed1512af46a302153199e03f863
cuuw619-maker <cuuw619@gmail.com>
2026-09-19 18:26:24 +0300

    Remove obsolete project layer

--- core/src/main/kotlin/com/imux/core/state/LauncherState.kt
+++
@@ -1,9 +0,0 @@
-package com.imux.core.state
-import com.imux.core.model.InstallationProfile
-sealed interface LauncherUiState {
-    data object Loading : LauncherUiState
-    data class Ready(val profile: InstallationProfile, val runtimeAvailable: Boolean) : LauncherUiState
-    data class Launching(val profile: InstallationProfile) : LauncherUiState
-    data class Running(val profile: InstallationProfile, val pid: Long?) : LauncherUiState
-    data class Error(val message: String) : LauncherUiState
-}
